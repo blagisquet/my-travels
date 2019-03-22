867ec5f1e7c2bfe1efa0f1c843c78b1dc07a68be
Baptiste Lagisquet <b.lagisquet@gmail.com>
2019-03-22 09:38:25 +0100

    part 2 cmpleted

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import Travel from "./Travel";
+import Travels from "./Travels";
 
 class App extends Component {
   render() {
@@ -12,18 +13,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Babou's travels </h1>
         </header>
-        <Travel
-          destination="Toronto"
-          country="Canada"
-          distance="6600"
-          photo="https://www.clc.fr/medias/_cache/produits/472/imagePrincipale/1920_1440/panorama-sejour-linguistique-toronto.jpg"
-        />
-        <Travel
-          destination="Cleveland"
-          country="USA"
-          distance="7500"
-          photo="https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Ffortunedotcom.files.wordpress.com%2F2018%2F07%2Fcleveland-ohio-fbi-arrest-july-4.jpg&w=1000&q=85"
-        />
+        {Travels.map(travel => (
+            <Travel photo={travel.photo} destination={travel.destination}
+                    country={travel.country} distance={travel.distance}/>
+        ))}
       </div>
     );
   }
---
+++ src/Travels.js
@@ -0,0 +1,36 @@
+const Travels = [
+  {
+    destination:
+      "Toronto",
+    country: "Canada",
+    photo:
+      "https://blog-and-the-city.com/wp-content/uploads/2015/10/toronto435435.jpg",
+    distance: "6000"
+  },
+  {
+    destination:
+      "Nantes",
+    country: "France",
+    photo:
+      "https://footballstories.konbini.com/files/2016/09/la-beaujoire-facebook-fc-nantes.jpg",
+    distance: "ICI"
+  },
+  {
+    destination:
+      "Uruguay",
+    country: "Uruguay",
+    photo:
+      "https://pvtistes.net/wp-content/uploads/2016/02/PVT-Uruguay.jpg",
+    distance: "8000"
+  },
+  {
+    destination:
+    "Salies de Bearn",
+  country: "Pays Basque",
+  photo:
+    "http://images.larepubliquedespyrenees.fr/2015/08/17/568158c5a43f5e4d408f7792/golden/salies-est-recommandee-comme-destination-interessante-par-le-guide-vert-michelin.jpg",
+  distance: "500"
+  }
+];
+
+export default Travels;
